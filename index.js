cd8fc094215e0d6eb0a013485f015eceb98e574b
Guilherme Colusso <guiihcolusso.dev@gmail.com>
2021-12-29 17:24:54 -0300

    Aula 1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Olá Mundo')
+const fs = require('fs') //File system
+
+fs.readFile('teste.txt', 'utf8', (err, data) => {
+
+    if (err){
+        console.log(err)
+    }
+
+    console.log(data)
+})
