4762d692672917bce223dc33beccf44dd8a1a223
Guilherme Colusso <guiihcolusso.dev@gmail.com>
2021-12-30 17:59:32 -0300

    Testing

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const chalk = require('chalk')
+const inquirer = require('inquirer')
 
-const nota = 9
+inquirer.prompt([{
+    name: 'n1', 
+    message: 'qual a Primeira Nota : '
+},{
+    name: 'n2', 
+    message: 'qual a Segunda Nota : '
+},{
+    name: 'n3', 
+    message: 'qual a Terceira Nota : '
+},{
+    name: 'n4', 
+    message: 'qual a Quarta Nota : '
+}]).then((answers) =>{
+
+    const total = parseFloat(answers.n1) + parseFloat(answers.n2) + parseFloat(answers.n3) + parseFloat(answers.n4)
+    const media = total / 4
+
+
+    console.clear()
+    console.log('Total das notas informadas : ', total)
+    console.log('Media das notas informadas : ', media)
+
+
+}).catch(err => console.log(err))
