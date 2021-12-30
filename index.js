794a34e068d0d2dd6ea2a9fa0fd5b6930aca2eb5
Guilherme Colusso <guiihcolusso.dev@gmail.com>
2021-12-30 01:04:59 -0300

    Projeto

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const minimist = require('minimist')
-const args = minimist(process.argv.slice(2))
+const chalk = require('chalk')
 
-const total = args['n1'] + args['n2'] + args['n3'] + args['n4']
-let media = total / 4
-
-console.log('Total : ', total, 'Media : ', media)
+const nota = 9
